4c8790c5595a96eedea3af24bbea25d199af3244
Shivam Chimra <shivam.chimra@infoedge.com>
2021-09-17 22:32:29 +0530

    first commit.........

--- script2.js
+++ script2.js
@@ -1,123 +1,117 @@
 let docImg;
+let userName = "";
 
+async function compareImg() {
+  let image;
+  let canvas;
 
-async function compareImg(){
-    let image
-    let canvas
-  
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-      if (image) image.remove()
-      if (canvas) canvas.remove()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+  if (image) image.remove();
+  if (canvas) canvas.remove();
 
+  const container = document.createElement("div");
+  container.style.position = "relative";
+  container.id = "id-1";
 
-    const container = document.createElement('div')
-    container.style.position = 'relative'
-    container.id="id-1";
-  
-    document.body.append(container)
-    
-    //const labeledFaceDescriptors = await loadLabeledImages('Black Widow');
-    image=docImg;
-   
+  document.body.append(container);
 
+  //const labeledFaceDescriptors = await loadLabeledImages('Black Widow');
+  image = docImg;
 
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
-    console.log(results)
+  container.append(image);
+  canvas = faceapi.createCanvasFromMedia(image);
+  container.append(canvas);
+  const displaySize = { width: image.width, height: image.height };
+  faceapi.matchDimensions(canvas, displaySize);
+  const detections = await faceapi
+    .detectAllFaces(image)
+    .withFaceLandmarks()
+    .withFaceDescriptors();
+  const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  const results = resizedDetections.map((d) =>
+    faceMatcher.findBestMatch(d.descriptor)
+  );
+  console.log(results);
 
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
+  results.forEach((result, i) => {
+    const box = resizedDetections[i].detection.box;
+    const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
+    drawBox.draw(canvas);
+  });
 }
 
-const video = document.getElementById('video')
+const video = document.getElementById("video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas2");
 var labeledFaceDescriptors;
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
 
-video.addEventListener('play', () => {
+video.addEventListener("play", () => {
   const canvas = faceapi.createCanvasFromMedia(video);
 
-  let videoContainer=document.getElementById("video-container")
-  videoContainer.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  let videoContainer = document.getElementById("video-container");
+  videoContainer.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-  }, 100)
-})
-click_button.addEventListener('click', async ()=> {
-  
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-  let image_data_url = canvas.toDataURL('image/jpeg');
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+  }, 100);
+});
+click_button.addEventListener("click", async () => {
+  userName = document.getElementById("name").value;
 
-  var img = document.createElement('img');
-            img.src=image_data_url
-            labeledFaceDescriptors = await loadLabeledImages('Shivam',img);
+  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+  let image_data_url = canvas.toDataURL("image/jpeg");
 
+  var img = document.createElement("img");
+  img.src = image_data_url;
+  labeledFaceDescriptors = await loadLabeledImages(userName, img);
 
-            compareImg();
-    document.getElementById("img-container").style.display="none"
-
-
-  //getBase64FromImageUrl(image_data_url);
-  // data url of the image
-  //console.log(image_data_url);
+  compareImg();
+  document.getElementById("img-container").style.display = "none";
 });
 
-
-
-
-
-const imageUpload = document.getElementById('imageUpload')
+const imageUpload = document.getElementById("imageUpload");
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-]).then(start)
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+]).then(start);
 
 async function start() {
-
- 
-
-  imageUpload.addEventListener('change', async () => {
-  
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    docImg=image
-    document.getElementById("img-container").style.display="block"
-    document.getElementById("doc-img").style.display="none"
-  })
+  imageUpload.addEventListener("change", async () => {
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
+    docImg = image;
+    document.getElementById("img-container").style.display = "block";
+    document.getElementById("doc-img").style.display = "none";
+  });
 }
 
-async function loadLabeledImages(name,img) {
-      const descriptions = []
-      const detections2 = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-      descriptions.push(detections2.descriptor)
-    
-      return new faceapi.LabeledFaceDescriptors(name,descriptions)
+async function loadLabeledImages(name, img) {
+  const descriptions = [];
+  const detections2 = await faceapi
+    .detectSingleFace(img)
+    .withFaceLandmarks()
+    .withFaceDescriptor();
+  descriptions.push(detections2.descriptor);
+
+  return new faceapi.LabeledFaceDescriptors(name, descriptions);
 }
